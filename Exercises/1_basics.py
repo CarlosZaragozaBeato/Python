ee2b48dae7c6faf2aeea2fb6d2c0d24d4cd419b1
Gbatz2 <you@example.com>
2022-03-26 15:39:38 +0100

    firstDay

---
+++ Exercises/1_basics.py
@@ -0,0 +1,111 @@
+"""
+1. Write a Python program to print the following string in a 
+specific format (see the output). Go to the editor
+Sample String : "Twinkle, twinkle, little star, 
+How I wonder what you are! Up above the world so high, Like a diamond 
+in the sky. Twinkle, twinkle, little star, How I wonder what you are" 
+Output :
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are
+"""
+
+
+
+
+
+"""
+2. Write a Python program to get the Python version you are using.
+"""
+
+
+
+
+
+"""
+3. Write a Python program to display the current date and time.
+Sample Output :
+Current date and time :
+2014-07-05 14:34:14
+"""
+
+
+
+
+"""
+4. Write a Python program which accepts 
+the radius of a circle from the user and 
+compute the area. 
+Sample Output :
+r = 1.1
+Area = 3.8013271108436504
+"""
+
+
+
+
+
+"""
+5. Write a Python 
+program which accepts the
+ user's first and last name and print 
+ them in reverse order with a space between them
+"""
+
+
+
+
+"""
+6. Write a Python program which accepts 
+a sequence of comma-separated numbers from 
+user and generate a list and a tuple with 
+those numbers. Go to the editor
+Sample data : 3, 5, 7, 23
+Output :
+List : ['3', ' 5', ' 7', ' 23']
+Tuple : ('3', ' 5', ' 7', ' 23')
+"""
+
+
+
+
+
+"""
+7. Write a Python program to accept a filename from the user and print the extension of that. Go to the editor
+Sample filename : abc.java
+Output : java
+"""
+
+
+
+
+"""
+8. Write a Python program to display 
+the first and last colors from the following 
+list. Go to the editor
+color_list = ["Red","Green","White" ,"Black"]
+"""
+
+
+
+"""
+9. Write a Python program to display 
+the examination schedule. (extract the 
+date from exam_st_date). Go to the editor
+exam_st_date = (11, 12, 2014)
+Sample Output : 
+The examination will start from : 11 / 12 / 2014
+"""
+
+
+
+"""
+10. Write a Python program that 
+accepts an integer (n) and computes 
+the value of n+nn+nnn. Go to the editor
+Sample value of n is 5
+Expected Result : 615
+"""
