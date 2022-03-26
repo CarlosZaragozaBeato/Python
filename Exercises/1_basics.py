358e170f57f454f8a104cd03063e5d83cb25b6d6
Gbatz2 <you@example.com>
2022-03-26 19:27:12 +0100

    4

--- Exercises/1_basics.py
+++ Exercises/1_basics.py
@@ -168,3 +168,205 @@ def Exercise10 ():
     print(value1+value2+value3)
 
 Exercise10()
+
+
+
+"""
+11. Write a Python program to print the documents
+(syntax, description etc.) of Python built-in function(s).
+Sample function : abs()
+Expected Result :
+abs(number) -> number
+Return the absolute value of the argument.
+"""
+
+
+def Exercise11():
+    number= float(input("Introduce a number: "))
+    try:
+        print(abs(number))
+    except:
+        print("Please introduce a number.")
+
+Exercise11()
+
+"""
+12. Write a Python program to print 
+the calendar of a given month and year.
+Note : Use 'calendar' module.
+"""
+
+
+
+
+
+
+"""
+13. Write a Python program to print the following 'here document'. Go to the editor
+Sample string :
+a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example
+"""
+
+
+
+
+
+
+"""
+14. Write a Python program to calculate number of days between two dates.
+Sample dates : (2014, 7, 2), (2014, 7, 11)
+Expected output : 9 days
+"""
+
+
+
+
+
+"""
+15. Write a Python program to get the volume of a sphere with radius 6.
+"""
+
+
+
+
+
+"""
+16. Write a Python program to 
+get the difference between a given
+number and 17, if the number is greater
+than 17 return double the absolute difference
+""" 
+
+
+
+
+
+"""
+17. Write a Python program to test whether
+a number is within 100 of 1000 or 2000.
+"""
+
+
+
+
+
+"""
+18. Write a
+Python program to calculate 
+the sum of three given numbers, 
+if the values are equal then return three 
+times of their sum.
+"""
+
+
+
+
+"""
+19. Write a Python program to get 
+a new string from a given string where 
+"Is" has been added to the front.
+If the given string already begins 
+with "Is" then return the string unchanged.
+"""
+
+
+
+
+
+"""
+20. Write a Python program to get a string which 
+is n (non-negative integer) copies of a given string.
+"""
+
+
+
+
+
+"""
+21. Write a Python program to find whether 
+a given number (accept from the user) 
+is even or odd, print out an appropriate message to the user.
+"""
+
+
+
+
+"""
+22. Write a Python program to count the number 4 in a given list.
+"""
+
+
+
+"""
+23. Write a Python program to get the n (non-negative integer) copies
+of the first 2 characters of a given string. 
+Return the n copies of the whole string if the 
+length is less than 2
+"""
+
+
+"""
+24. Write a Python 
+program to test whether a passed 
+letter is a vowel or not.
+"""
+
+
+
+
+"""
+25. Write a Python program to check whether a specified
+ value is contained in a group of values.
+"""
+
+
+"""
+26. Write a Python program to create a
+histogram from a given list of integers.
+"""
+
+
+
+"""
+27. Write a Python program to concatenate
+all elements in a list into a string and return it.
+"""
+
+
+"""
+28. Write a Python program to print all even numbers
+from a given numbers list in the same order and stop the printing if any numbers that come after 237 in the sequence. Go to the editor
+Sample numbers list :
+
+numbers = [    
+    386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 
+	597, 978, 328, 615, 953, 345, 
+    399, 162, 758, 219, 918, 237, 412, 566, 826, 
+	248, 866, 950, 626, 949, 687, 217, 
+    815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 
+	81, 379, 843, 831, 445, 742, 717, 
+    958,743, 527
+    ]
+"""
+
+
+
+
+
+"""
+29. Write a Python program to print out a set containing all the colors from color_list_1 which are not present in color_list_2. Go to the editor
+Test Data :
+color_list_1 = set(["White", "Black", "Red"])
+color_list_2 = set(["Red", "Green"])
+Expected Output :
+{'Black', 'White'}
+"""
+
+
+"""
+30. Write a Python program that will accept the base and
+height of a triangle and compute the area.
+"""
