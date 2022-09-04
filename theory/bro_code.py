1eee8d6f6e51ec4b1b4d6adb0c2872af88b7a706
Carlos <carloszaragozabeato15@gmail.com>
2022-09-04 17:21:38 +0200

    finish today

--- theory/bro_code.py
+++ theory/bro_code.py
@@ -499,3 +499,273 @@ Car.wheels = 2
 print(car_1.wheels)
 print(car_2.wheels)
 """
+#inheritance 
+"""
+class Animal: 
+    alive = True
+    def eat(self):
+        print("This animal is eating")
+    
+    def slumber(self):
+        print("This animal is sleeping")
+
+class Rabbit(Animal):
+    def run(self):
+        print("This animal is running")
+        
+class Fish(Animal):
+    def swim(self):
+        print("This animal is swimming")
+        
+rabbit = Rabbit()
+fish = Fish()
+animal = Animal()
+
+rabbit.run()
+fish.swim()
+animal.slumber()
+animal.eat()
+"""
+#Multilevel inheritance
+"""
+class Organism:
+    alive = True
+    def isAlive(self):
+        print("This organism is alive")
+        
+class Animal(Organism):
+    def eat(self):
+        print("This animal is eating")
+    
+class Dog(Animal):
+    def bark(self):
+        print("This dog is barking")
+    
+organism = Organism()
+organism.isAlive()
+
+animal = Animal()
+animal.isAlive()
+
+dog = Dog()
+dog.bark()
+"""
+#Multiple inheritance
+"""
+class Prey:
+    def flee(self):
+        print("This animal flees")
+        
+class Predator:
+    def hunt(self):
+        print("This animal is hunting")
+        
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+rabbit = Rabbit()
+hawk = Hawk()
+
+rabbit.flee()
+hawk.hunt()
+"""
+#Method overriding
+"""
+class Animal:
+    def eat(self):
+        print("This animal is eating")
+        
+class Rabbit(Animal):
+    def eat(self):
+        print("This rabbit is eating")
+
+rabbit = Rabbit()
+rabbit.eat()
+"""
+
+#Chaining methods
+"""
+method chaining = calling multiple methods sequentially 
+        each call performs an action on the same object and returns self
+"""
+"""
+class Car:
+    
+    def turn_on(self):
+        print("You start the engine")
+        return self
+    
+    def drive(self):
+        print("You drive the car")
+        return self
+    
+    def brake(self):
+        print("You step on the brakes")
+        return self
+    
+    def turn_off(self):
+        print("You turn off the car")
+        return self
+
+car = Car()
+car.turn_on().drive().brake().turn_off()
+"""
+#super function
+"""
+super() = Function used to give access to the methods of a parent class
+Returns a temporary object of a parent class when used
+"""
+"""
+class Rectangle:
+    def __init__ (self, length, width):
+        self.length = length
+        self.width = width
+        
+class Square(Rectangle):
+    def __init__(self, length, width):
+        super().__init__(length, width)
+        
+    def area(self):
+            return self.length * self.width
+
+class Cube(Rectangle):
+    def __init__(self, length, width, height):
+        super().__init__(length, width)
+        self.height = height
+        
+    def volumn(self):
+            return self.length * self.width * self.height
+
+square = Square(3,3)
+cube = Cube(3,3,3)
+
+print(square.area())
+print(cube.volumn())
+"""
+"""
+Prevents a user from creating an object of that class
+    compels a user to override abstract methods in a child class
+    
+    abstract class - a class which contains one or more abstract methods
+    abstract method - a method that has a declaration but does not have an implementation
+    
+"""#Abstract classes
+"""
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    
+    @abstractmethod
+    def go(self):
+        pass
+    
+    @abstractmethod
+    def stop(self):
+        pass
+    
+
+class Car(Vehicle):
+    def go (self):
+        print("You drive the car")
+    
+    def stop(self):
+        print("This car is stopped")
+        
+class Motorcycle(Vehicle):
+    def go(self):
+        print("You ride the motorcycle")
+        
+    def stop(self):
+        print("This motorcycle is stopped")
+        
+
+
+
+car = Car()
+motorcycle = Motorcycle()
+
+car.go()
+car.stop()
+motorcycle.go()
+motorcycle.stop()
+"""
+
+""" # as arguments
+class Car:
+    color = None
+    
+class Motorcycle:
+    color = None
+    
+def change_color(vehicle, color):
+        vehicle.color = color
+        
+car_1 = Car()
+car_2 = Car() 
+car_3 = Car()
+
+change_color(car_1, "red")
+change_color(car_2, "White")
+change_color(car_3, "Yellow")
+
+print(car_1.color)
+print(car_2.color)
+print(car_3.color)
+"""
+
+"""
+Duck typing = concept where the class of an object is less importan than the methods
+class type is not checked if minimum methods/attributes are present 
+"If it walks like a duck, and it quacks like a duck, then it must be a duck"
+class Duck:
+    def walk(self):
+        print("This duck is walking")
+        
+    def talk(self):
+        print("This duck is qwuacking")
+        
+class Chicken:
+    def walk(self):
+        print("This chicken is walking")
+        
+    def talk(self):
+        print("This chicken is clucking")
+        
+class Person():
+    def catch(self, duck):
+        duck.walk()
+        duck.talk()
+        print("You caught the critter") 
+        
+duck = Duck()
+chicken = Chicken()
+person = Person()
+
+duck.talk()
+duck.walk()
+chicken.talk()
+person.catch(duck)
+"""
+
+"""#Walrus operator
+
+walrus operator :=
+
+new to Python 3.8
+assignment expression aka walrus operator
+assigns values to variables a part of a large expression
+
+foods = lists()
+while True:
+ food = input("What food do you like?:")
+    if food == "quit":
+        break
+    food.append(food)
+foods = list()
+while food := input("What food do yoy like") != "quit":
+    foods.append(food) 
+print(foods)
+"""
