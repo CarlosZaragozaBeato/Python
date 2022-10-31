748e02597895d8bdeb7f3b870670a0b7e4e36a3f
GbatZ2 <carloszaragozabeato15@gmail.com>
2022-10-31 17:10:47 +0100

    finished

--- theory/funciones.py
+++ theory/funciones.py
@@ -2,89 +2,14 @@ from random import randint
 import math
 """
 Current Exercises
-Ejercicio 14
-Vamos a crear un programa para trabajar con una pila. Una pila es una estructura de datos que nos permite guardar un conjunto de variables.
-La característica fundamental es que el último elemento que se añade al conjunto es el primero que se puede sacar.
-
-Para representar una pila vamos a utilizar una lista de cadenas de caracteres.
-
-Vamos a crear varias funciones para trabajar con la pila:
-
-LongitudPila: Función que recibe una pila y devuelve el número de elementos que tiene.
-EstaVaciaPila: Función que recibe una pila y que devuelve si la pila está vacía, no tiene elementos.
-EstaLlenaPila: Función que recibe una pila y que devuelve si la pila está llena.
-AddPila: función que recibe una cadena de caracteres y una pila, y añade la cadena a la pila, si no está llena. si esta llena muestra un mensaje de error.
-SacarDeLaPila: Función que recibe una pila y devuelve el último elemento añadido y lo borra de la pila. Si la pila está vacía muestra un mensaje de error.
-EscribirPila: Función que recibe una pila y muestra en pantalla los elementos de la pila.
-Realiza un programa principal que nos permita usar las funciones anterior, que nos muestre un menú, con las siguientes opciones:
-
-Añadir elemento a la pila
-Sacar elemento de la pila
-Longitud de la pila
-Mostrar pila
-Salir
 """
-
-
-lista = []
-
-def Longitud_Pila(lista):
-    return len(lista)
-
-def EstaVaciaPila(lista):
-    if len(lista) == 0:
-        return True
-    else: return False
-
-def EstaLlenaPila(lista):
-    if len(lista) == 15:
-        return True
-    else: False
-
-def AddPila(cadena, lista):
-    if EstaLlenaPila(lista):
-        return "La pila esta llena"
-    else: 
-        return lista.append(cadena)
-
-def SacarDePila(cadena, lista):
-    if EstaVaciaPila(lista):
-        return "La pila esta vacia"
-    else:
-        lista.remove(cadena)
-
-cadena = "CARLOS"
-
-
-
-def MostrarPila(lista):
-    if EstaVaciaPila(lista):
-        return "La pila esta vacia"
-    else:
-        for i in lista:
-            print(i)
-
-
-
-
-AddPila(cadena, lista)
-
-print(lista)
-
-SacarDePila(cadena,lista)
-
-print(len(lista))
-
 
 
 
 
 
 """
-Ejercicio 15
-Vamos a realizar un programa similar al anterior para trabajar con una cola. Una cola es una estructura de datos que nos permite guardar un conjunto de variables. La característica fundamental es que el primer elemento que se añade al conjunto es el primero que se puede sacar.
 
-En realizada nos sirven todas las funciones del ejercicio anterior menos la función SacarDeLaCola que es la que tienes que modificar.
 """
 
 """ Completed
@@ -458,4 +383,129 @@ def Multiplicar_Fracciones(n1,n2,d1,d2):
     numerador = n1 * n2
     denominador = d1 * d2
     return Simplificar_Fraccion(numerador, denominador)
+*******************************
+Ejercicio 14
+Vamos a crear un programa para trabajar con una pila. Una pila es una estructura de datos que nos permite guardar un conjunto de variables.
+La característica fundamental es que el último elemento que se añade al conjunto es el primero que se puede sacar.
+
+Para representar una pila vamos a utilizar una lista de cadenas de caracteres.
+
+Vamos a crear varias funciones para trabajar con la pila:
+
+LongitudPila: Función que recibe una pila y devuelve el número de elementos que tiene.
+EstaVaciaPila: Función que recibe una pila y que devuelve si la pila está vacía, no tiene elementos.
+EstaLlenaPila: Función que recibe una pila y que devuelve si la pila está llena.
+AddPila: función que recibe una cadena de caracteres y una pila, y añade la cadena a la pila, si no está llena. si esta llena muestra un mensaje de error.
+SacarDeLaPila: Función que recibe una pila y devuelve el último elemento añadido y lo borra de la pila. Si la pila está vacía muestra un mensaje de error.
+EscribirPila: Función que recibe una pila y muestra en pantalla los elementos de la pila.
+Realiza un programa principal que nos permita usar las funciones anterior, que nos muestre un menú, con las siguientes opciones:
+
+Añadir elemento a la pila
+Sacar elemento de la pila
+Longitud de la pila
+Mostrar pila
+Salir
+
+
+lista = []
+
+def Longitud_Pila(lista):
+    return len(lista)
+
+def EstaVaciaPila(lista):
+    if len(lista) == 0:
+        return True
+    else: return False
+
+def EstaLlenaPila(lista):
+    if len(lista) == 15:
+        return True
+    else: False
+
+def AddPila(cadena, lista):
+    if EstaLlenaPila(lista):
+        return "La pila esta llena"
+    else: 
+        return lista.append(cadena)
+
+def SacarDePila(cadena, lista):
+    if EstaVaciaPila(lista):
+        return "La pila esta vacia"
+    else:
+        lista.remove(cadena)
+
+cadena = "CARLOS"
+
+
+
+def MostrarPila(lista):
+    if EstaVaciaPila(lista):
+        return "La pila esta vacia"
+    else:
+        for i in lista:
+            print(i)
+
+
+
+
+AddPila(cadena, lista)
+
+print(lista)
+
+SacarDePila(cadena,lista)
+
+print(len(lista))
+
+*******************************
+Ejercicio 15
+Vamos a realizar un programa similar al anterior para trabajar con una cola. Una cola es una estructura de datos que nos permite guardar un conjunto de variables. La característica fundamental es que el primer elemento que se añade al conjunto es el primero que se puede sacar.
+
+En realizada nos sirven todas las funciones del ejercicio 
+anterior menos la función SacarDeLaCola que es la que tienes que modificar.
+
+
+def LongitudCola(cola):
+    return len(cola)
+
+def EstaVaciaCola(cola):
+    return len(cola) == 0
+
+def AddCola(cad,cola):
+    cola.append(cad)
+
+def SacarDeLaCola(cola):
+    if not EstaVaciaCola(cola):
+        return cola.pop(0)
+    else:
+        print("No se puede sacar elemento. La cola esta vacia.")
+        return ""
+    
+def EscribirCola(cola):
+    for elem in cola:
+        print(elem, end =" ")
+    print()
+    
+micola = []
+while True:
+	print("1.- Añadir elemento a la cola")
+	print("2.- Sacar elemento de la cola")
+	print("3.- Longitud de la cola")
+	print("4.- Mostrar cola")
+	print("5.- Salir")
+	opcion = int(input())
+	if opcion == 1:
+		elem = input("Dame la cadena para añadir a la cola:")
+		AddCola(elem,micola)
+	elif opcion == 2:
+		print(SacarDeLaCola(micola))
+	elif opcion == 3:
+		print("Longitud: ",LongitudCola(micola))
+	elif opcion == 4:
+		EscribirCola(micola)
+	elif opcion == 5:
+		break
+	else:
+		print("Opción incorrecta")
+	
+
 """
