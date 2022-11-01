ac59ee1c11eced1cd8adf6752478ef91524c08cd
GbatZ2 <carloszaragozabeato15@gmail.com>
2022-11-01 05:52:55 +0100

    asd

---
+++ theory/clases.py
@@ -0,0 +1,152 @@
+import math 
+import random
+
+class Cuenta:
+    titular = ""
+    cantidad = 0
+    def __init__(self, titular):
+        self.titular = titular
+        
+    def __init__(self) -> None:
+        pass
+    
+    
+    def setTitular(self,titular):
+        self.titular = titular
+    
+    def getTitulat(self):
+        return self.titular
+
+    def Retirar_Cantidad(self, cantidad):
+        self.cantidad -= cantidad
+    
+    def Ingresar_Cantidad(self, cantidad):
+        self.cantidad += cantidad
+    
+    def mostrar(self):
+        print(f"Titulas: {self.titular}, cantidad: {self.cantidad}")
+        
+class CuentaJoven(Cuenta):
+
+    def __init__(self,titular,cantidad=0,bonificacion=0):
+        super().__init__(titular,cantidad)
+        self.bonificacion=bonificacion
+    
+    @property
+    def bonificacion(self):
+        return self.__bonificacion
+    
+    @bonificacion.setter
+    def bonificacion(self,bonificacion):
+        self.__bonificacion=bonificacion
+
+    def mostrar(self):
+        return "Cuenta Joven\n"+"Titular:"+self.titular.mostrar()+" - Cantidad:"+str(self.cantidad)+ "- Bonificación:"+str(self.bonificacion)+"%"
+   
+    def esTitularValido(self):
+        return self.titular.edad < 25 and self.titular.esMayorDeEdad()
+    
+    def retirar(self,cantidad):
+        if not self.esTitularValido():
+            print ("No puesdes retirar el dinero. titular no válido")
+        elif cantidad > 0:
+            super().retirar(cantidad)
+    
+    
+""" 
+
+
+Ejercicio 2
+Crea una clase llamada Cuenta que tendrá los siguientes atributos: titular (que es una persona) y cantidad (puede tener decimales).
+El titular será obligatorio y la cantidad es opcional. Construye los siguientes métodos para la clase:
+
+Un constructor, donde los datos pueden estar vacíos.
+Los setters y getters para cada uno de los atributos. El atributo no se puede modificar directamente, solo ingresando o retirando dinero.
+mostrar(): Muestra los datos de la cuenta.
+ingresar(cantidad): se ingresa una cantidad a la cuenta, si la cantidad introducida es negativa, no se hará nada.
+retirar(cantidad): se retira una cantidad a la cuenta. La cuenta puede estar en números rojos.
+
+
+Ejercicio 3
+Vamos a definir ahora una “Cuenta Joven”, para ello vamos a crear una nueva clase CuentaJoven que deriva de la anterior. Cuando se crea esta 
+nueva clase, además del titular y la cantidad se debe guardar una bonificación que estará expresada en tanto por ciento.Construye 
+los siguientes métodos para la clase:
+
+Un constructor.
+Los setters y getters para el nuevo atributo.
+En esta ocasión los titulares de este tipo de cuenta tienen que ser mayor de edad., por lo tanto hay que crear un método esTitularValido() que devuelve
+verdadero si el titular es mayor de edad pero menor de 25 años y falso en caso contrario.
+Además la retirada de dinero sólo se podrá hacer si el titular es válido.
+El método mostrar() debe devolver el mensaje de “Cuenta Joven” y la bonificación de la cuenta.
+Piensa los métodos heredados de la clase madre que hay que reescribir.
+
+
+"""
+
+""" 
+Ejercicio 1
+Vamos a crear una clase llamada Persona. Sus atributos son: nombre, edad y DNI. Construye los siguientes métodos para la clase:
+
+Un constructor, donde los datos pueden estar vacíos.
+Los setters y getters para cada uno de los atributos. Hay que validar las entradas de datos.
+mostrar(): Muestra los datos de la persona.
+esMayorDeEdad(): Devuelve un valor lógico indicando si es mayor de edad.
+
+
+
+
+class Persona:
+    nombre = ""
+    edad = None
+    dni = ""
+    
+    def __init__(self,nombre, edad, dni):
+        self.nombre = nombre
+        self.edad = edad
+        self.dni = dni
+    
+    
+    def getNombre(self):
+        return self.nombre
+    
+    def getEdad(self):
+        return self.edad
+    
+    def getDni(self):
+        return self.dni
+    
+    def setNombre(self,nombre):
+        self.nombre = nombre
+        
+    def setEdad(self,edad):
+        self.edad = edad
+        
+    def setDni(self, dni):
+        self.dni = dni
+        
+    def toString(self):
+        return f"Nombre: {self.nombre} edad: {self.edad} dni: {self.dni}"
+    
+    def esMayorDeEdad(self):
+        if self.edad>=18:
+            return True
+        else: return False
+        
+persona = Persona("carlos", 23, "70361041q")
+print(persona.toString())
+print(persona.esMayorDeEdad())
+
+persona.setDni("1")
+persona.setEdad(24)
+persona.setNombre("Carlos2")
+
+print(persona.toString())
+
+print(persona.getDni())
+print(persona.getEdad())
+print(persona.getNombre())
+
+
+*********************************
+
+"""
