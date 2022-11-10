950819a7e7e24ad516fd3968a473d6d44062b141
GbatZ2 <carloszaragozabeato15@gmail.com>
2022-11-10 06:14:03 +0100

    asd

--- 100Ex/Ex100.py
+++ 100Ex/Ex100.py
@@ -2,138 +2,42 @@ import random
 import math
 
 
+accion = input("Introduce una opcion (D, W)")
+quantity = int(input("Introduce the corresponding quantity: "))
+amount = 0
+while True:
 
-
-
-    
-    
-
-
+    if accion =="":break
+    elif amount>0 and int(quantity)<=amount and accion.upper() == "W": 
+        amount-= int(quantity)
+    elif accion.upper() == "D":
+        amount += int(quantity)
+    accion = input("Introduce una opcion (D, W)")
+    quantity = input("Introduce the corresponding quantity: ")
+print(amount)
 
 
 """
 
-Question 11
-Level 2
-
-Question: Write a program which accepts a sequence of comma separated 4 digit binary 
-numbers as its input and then check whether they are divisible by 5 or not. The numbers
-that are divisible by 5 are to be printed in a comma separated sequence. Example: 0100,0011,1010,1001 
-Then the output should be: 1010 Notes: Assume the data is input by console.
-
-Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
 
 
 
-
-
-
-
-
-
-
-Solution:
-
-value = []
-items=[x for x in input().split(',')]
-for p in items:
-    intp = int(p, 2)
-    if not intp%5:
-        value.append(p)
-
-print(','.join(value))
-Question 12
-Level 2
-
-Question: Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the number is an even number. The numbers obtained should be printed in a comma-separated sequence on a single line.
-
-Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
-
-Solution:
-
-values = []
-for i in range(1000, 3001):
-    s = str(i)
-    if (int(s[0])%2==0) and (int(s[1])%2==0) and (int(s[2])%2==0) and (int(s[3])%2==0):
-        values.append(s)
-print(",".join(values))
-Question 13
-Level 2
-
-Question: Write a program that accepts a sentence and calculate the number of letters and digits. Suppose the following input is supplied to the program: hello world! 123 Then, the output should be: LETTERS 10 DIGITS 3
-
-Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
-
-Solution:
-
-s = input()
-d={"DIGITS":0, "LETTERS":0}
-for c in s:
-    if c.isdigit():
-        d["DIGITS"]+=1
-    elif c.isalpha():
-        d["LETTERS"]+=1
-    else:
-        pass
-print("LETTERS", d["LETTERS"])
-print("DIGITS", d["DIGITS"])
-Question 14
-Level 2
-
-Question: Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters. Suppose the following input is supplied to the program: Hello world! Then, the output should be: UPPER CASE 1 LOWER CASE 9
-
-Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
-
-Solution:
-
-s = input()
-d={"UPPER CASE":0, "LOWER CASE":0}
-for c in s:
-    if c.isupper():
-        d["UPPER CASE"]+=1
-    elif c.islower():
-        d["LOWER CASE"]+=1
-    else:
-        pass
-print("UPPER CASE", d["UPPER CASE"])
-print("LOWER CASE", d["LOWER CASE"])
-Question 15
-Level 2
-
-Question: Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a. Suppose the following input is supplied to the program: 9 Then, the output should be: 11106
-
-Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
-
-Solution:
-
-a = input()
-n1 = int( "%s" % a )
-n2 = int( "%s%s" % (a,a) )
-n3 = int( "%s%s%s" % (a,a,a) )
-n4 = int( "%s%s%s%s" % (a,a,a,a) )
-print(n1+n2+n3+n4)
-Question 16
-Level 2
-
-Question: Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers. Suppose the following input is supplied to the program: 1,2,3,4,5,6,7,8,9 Then, the output should be: 1,3,5,7,9
-
-Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
-
-Solution:
-
-values = input()
-numbers = [x for x in values.split(",") if int(x)%2!=0]
-print(",".join(numbers))
 Question 17
 Level 2
 
-Question: Write a program that computes the net amount of a bank account based a transaction log from console input. The transaction log format is shown as following: D 100 W 200
+Question: Write a program that computes the net amount of a bank account based a transaction log from console input.
+The transaction log format is shown as following: D 100 W 200
 
-D means deposit while W means withdrawal. Suppose the following input is supplied to the program: D 300 D 300 W 200 D 100 Then, the output should be: 500
+D means deposit while W means withdrawal. Suppose the following input is supplied to the program:
+D 300 D 300 W 200 D 100 Then, the output should be: 500
 
 Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
 
-Solution:
+Solutio
+
+
+
+n:
 
 netAmount = 0
 while True:
@@ -1856,4 +1760,137 @@ Solution:
 s = input()
 words = [word for word in s.split(" ")]
 print(" ".join(sorted(list(set(words)))))
+
+*******************************
+Question 11
+Level 2
+
+Question: Write a program which accepts a sequence of comma separated 4 digit binary 
+numbers as its input and then check whether they are divisible by 5 or not. The numbers
+that are divisible by 5 are to be printed in a comma separated sequence. Example: 0100,0011,1010,1001 
+Then the output should be: 1010 Notes: Assume the data is input by console.
+
+Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
+
+valores = []
+items = [x for x in input().split(",")]
+for p in items:
+    intp = int(p,2)
+    if not intp%5:
+        valores.append(p)
+print(','.join(valores))
+***********************
+
+Question 12
+Level 2
+
+Question: Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the number is an even number. 
+The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
+
+
+valores = []
+
+for i in range(1000, 3001):
+    s = str(i)
+    if (int(s[0])%2==0) and (int(s[1])%2==0) and (int(s[2])%2==0) and (int(s[3])%2==0):
+        valores.append (s)
+    print(','.join(valores))
+***********************
+
+Question 13
+Level 2
+
+Question: Write a program that accepts a sentence and calculate the number of letters and digits. Suppose the following input
+is supplied to the program: hello world! 123 Then, the output should be: LETTERS 10 DIGITS 3
+
+Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
+
+cadena = input("Introduce una cadena: ")
+
+alpha = 0
+digit = 0
+
+for i in cadena:
+    if i.isdigit():
+        digit +=1
+    elif i.isalpha():
+        alpha +=1
+print(f"The number of letter is {alpha} and the number of digit is {digit}")
+
+*******************************
+
+
+Question 14
+Level 2
+
+Question: Write a program that accepts a sentence and calculate the number 
+of upper case letters and lower case letters. Suppose the following input is
+supplied to the program: Hello world! Then, the output should be: UPPER CASE 1 LOWER CASE 9
+
+Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
+
+Solution:
+character = "a"
+
+cadena = input("Introduce una cadena: ")
+uppers = 0
+lowers = 0
+
+for i in cadena:
+    if i.isupper():
+        uppers += 1
+    elif i.islower():
+        lowers += 1
+print(f"The number of letters in uppercase is {uppers}, and the number of letters in lowercase is {lowers}")
+*******************************
+Question 15
+Level 2
+
+Question: Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+Suppose the following input is supplied to the program: 9 Then, the output should be: 11106
+
+Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
+
+
+
+
+numero = "9" #(input("Introduce un numero para reemplazar: "))
+
+numero_nuevo = int(character.replace(character,numero))
+
+operation = numero_nuevo + int(f"{numero_nuevo}{numero_nuevo}")+int(f"{numero_nuevo}{numero_nuevo}{numero_nuevo}")+int(f"{numero_nuevo}{numero_nuevo}{numero_nuevo}{numero_nuevo}")
+print(operation)
+
+
+Solution:
+
+a = input()
+n1 = int( "%s" % a )
+n2 = int( "%s%s" % (a,a) )
+n3 = int( "%s%s%s" % (a,a,a) )
+n4 = int( "%s%s%s%s" % (a,a,a,a) )
+print(n1+n2+n3+n4)
+Question 16
+*******************************
+
+Level 2
+
+Question: Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers.
+Suppose the following input is supplied to the program: 1,2,3,4,5,6,7,8,9 Then, the output should be: 1,3,5,7,9
+
+Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
+
+
+
+
+Solution:
+
+
+valores = input("Introduce valores separados por comas: ")
+numeros = [x for x in valores.split(",") if int(x)%2!=0]
+print(",".join(numeros))
+
+
 """
