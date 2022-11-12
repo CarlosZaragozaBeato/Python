97c4b43608761f37033952d39ca2b01210313c53
GbatZ2 <carloszaragozabeato15@gmail.com>
2022-11-12 07:49:21 +0100

    str

--- 100Ex/Ex100.py
+++ 100Ex/Ex100.py
@@ -1,115 +1,12 @@
 import random 
 import math
 
-    
+
 
 
 
 
 """
-
-    
-
-
-
-Question 35
-Define a function which can generate a dictionary where the keys are numbers between 1 and 20 (both included) and the values are square of keys. The function should just print the values only.
-
-Hints:
-
-Use dict[key]=value pattern to put entry into a dictionary. Use ** operator to get power of a number. Use range() for loops. Use keys() to iterate keys in the dictionary. Also we can use item() to get key/value pairs.
-
-Solution
-
-def printDict():
-	d=dict()
-	for i in range(1,21):
-		d[i]=i**2
-	for (k,v) in d.items():	
-		print(v)
-
-printDict()
-Question 36
-Define a function which can generate a dictionary where the keys are numbers between 1 and 20 (both included) and the values are square of keys. The function should just print the keys only.
-
-Hints:
-
-Use dict[key]=value pattern to put entry into a dictionary. Use ** operator to get power of a number. Use range() for loops. Use keys() to iterate keys in the dictionary. Also we can use item() to get key/value pairs.
-
-Solution
-
-def printDict():
-	d=dict()
-	for i in range(1,21):
-		d[i]=i**2
-	for k in d.keys():	
-		print(k)
-
-printDict()
-Question 37
-Define a function which can generate and print a list where the values are square of numbers between 1 and 20 (both included).
-
-Hints:
-
-Use ** operator to get power of a number. Use range() for loops. Use list.append() to add values into a list.
-
-Solution
-
-def printList():
-	li=list()
-	for i in range(1,21):
-		li.append(i**2)
-	print(li)
-
-printList()
-Question 38
-Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). Then the function needs to print the first 5 elements in the list.
-
-Hints:
-
-Use ** operator to get power of a number. Use range() for loops. Use list.append() to add values into a list. Use [n1:n2] to slice a list
-
-Solution
-
-def printList():
-	li=list()
-	for i in range(1,21):
-		li.append(i**2)
-	print(li[:5])
-
-printList()
-Question 39
-Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). Then the function needs to print the last 5 elements in the list.
-
-Hints:
-
-Use ** operator to get power of a number. Use range() for loops. Use list.append() to add values into a list. Use [n1:n2] to slice a list
-
-Solution
-
-def printList():
-	li=list()
-	for i in range(1,21):
-		li.append(i**2)
-	print(li[-5:])
-
-printList()
-Question 40
-Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). Then the function needs to print all values except the first 5 elements in the list.
-
-Hints:
-
-Use ** operator to get power of a number. Use range() for loops. Use list.append() to add values into a list. Use [n1:n2] to slice a list
-
-Solution
-
-def printList():
-	li=list()
-	for i in range(1,21):
-		li.append(i**2)
-	print li[5:]
-
-printList()
 Question 41
 Define a function which can generate and print a tuple where the value are square of numbers between 1 and 20 (both included).
 
@@ -1900,4 +1797,114 @@ def DobleKeys():
 
 for key, value in DobleKeys().items():
     print(f"{key} : {value}")
+*******************************
+Question 35
+Define a function which can generate a dictionary where the keys are numbers between 1 and 20 (both included) and the values are square of keys. 
+The function should just print the values only.
+
+Hints:
+
+Use dict[key]=value pattern to put entry into a dictionary. Use ** operator to get power of a number. Use range() for loops. Use keys() to iterate keys in the dictionary. Also we can use item() to get key/value pairs.
+
+Solution
+
+def GenerateDictSquare():
+    dictionary = {}
+    for i in range(0,21):
+        dictionary[i] = i**2
+    return dictionary
+
+for value in GenerateDictSquare().values():
+    print(value)    
+*******************************
+Question 36
+Define a function which can generate a dictionary where the keys are numbers between 1 and 20 (both included) and 
+the values are square of keys. The function should just print the keys only.
+
+Hints:
+
+Use dict[key]=value pattern to put entry into a dictionary. Use ** operator to get power of a number. Use range() for loops. Use keys() to iterate keys in the dictionary. Also we can use item() to get key/value pairs.
+
+Solution
+def GenerateDictSquare():
+    dictionary = {}
+    for i in range(0,21):
+        dictionary[i] = i**2
+    return dictionary
+
+for key in GenerateDictSquare().keys():
+    print(key)
+*******************************
+Question 37
+Define a function which can generate and print a list where the values are square of numbers between 1 and 20 (both included).
+
+Hints:
+
+Use ** operator to get power of a number. Use range() for loops. Use list.append() to add values into a list.
+
+Solution
+
+def ListOneToTwenty():
+    list = []
+    for i in range(0,21):
+        list.append(i**2)
+    return list
+
+for i in ListOneToTwenty():
+    print(i)
+*******************************
+Question 38
+Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). 
+Then the function needs to print the first 5 elements in the list.
+
+Hints:
+
+Use ** operator to get power of a number. Use range() for loops. Use list.append() to add values into a list. Use [n1:n2] to slice a list
+
+Solution
+
+def ListTwenty():
+    list = []
+    for i in range(0,21):
+        list.append(i**2)
+
+    print(list[:5])
+        
+ListTwenty()
+*******************************
+Question 39
+Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included).
+Then the function needs to print the last 5 elements in the list.
+
+Hints:
+
+Use ** operator to get power of a number. Use range() for loops. Use list.append() to add values into a list. Use [n1:n2] to slice a list
+
+Solution
+
+def GenerateListSquare():
+    list = []
+    for i in range(0,21):
+        list.append(i**2)
+    print(list[-5:])
+    
+GenerateListSquare()
+*******************************
+Question 40
+Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included).
+Then the function needs to print all values except the first 5 elements in the list.
+
+Hints:
+
+Use ** operator to get power of a number. Use range() for loops. Use list.append() to add values into a list. Use [n1:n2] to slice a list
+
+Solution
+
+def GenerateList():
+    list = []
+    for i in range(0,21):
+        list.append(i**2)
+    print(list[5:])
+
+GenerateList()
 """
