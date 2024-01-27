e5cd66555a38437c238b247518050f6d7e95128c
Carlos Zaragoza Beato <carloszaragozabeato15@gmail.com>
2024-01-27 20:36:15 +0100

    s

---
+++ main.py
@@ -0,0 +1,15 @@
+import os 
+
+# TODO
+# test en leetcode
+# test de prueba
+# django con plotly
+# fastapi
+# flask
+
+
+def test(data):
+    print(data)
+
+if __name__ == "__main__":
+    test("carlao")
